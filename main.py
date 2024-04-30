0b92e5b1aafd8fbe6c5b290dee6d8c284528f1c2
sarahabdulkader <sarah.abdul.kader@univie.ac.at>
2024-04-30 11:11:48 +0200

    submit assessment

--- main.py
+++ main.py
@@ -2,12 +2,33 @@
 Simple Calculator
 """
 # Provide your solution here
+def calculate(a:int, b:int, operator:str) -> int and int and str:
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/" and b == 0:
+        print("Division by 0 is not allowed.")
+    elif operator == "/":
+        return a / b
+    else:
+        print("Invalid operator!")
+
+print(calculate(9, 6, "/"))
 
 
 """
 Reverse Word
 """
 # Provide your solution here
+
+def reverse_word(name: str) -> str:
+    return name[::-1].capitalize()
+print(reverse_word("SaRaH"))
+
+"""
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
